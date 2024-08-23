599719b43d9bcbe9859b1f636cbbea1790253004
NgocDiep2004 <ngoccdiep2004@gmail.com>
2024-08-23 10:20:46 +0700

    Add files via upload

---
+++ Diep.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(void){
+    // Your code here!
+    int chieudai = 5;
+    int chieurong =4;
+    printf("\nChieu dai hinh chu nhat la %d ", chieudai);
+    printf("\nChieu rong hinh chu nhat la %d ", chieurong);
+    int dientich = chieudai*chieurong;
+    printf("\nDien tich hinh chu nhat la: %d", dientich);
+    return dientich;
+}
